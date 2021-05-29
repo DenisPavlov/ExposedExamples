fdf10f3d92ce535b577d18d1ae1eeea2166936a1
denis <denispavlov1989@gmail.com>
2021-05-29 17:52:02 +0700

    add dependensies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,16 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    val exposedVersion = "0.31.1"
+    val kotestVersion = "4.4.0"
+
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    implementation("com.h2database:h2:1.4.197")
+
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+}
